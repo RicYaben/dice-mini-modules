72eb3998876fa32485bfe30079fcea2fb4c944ca
RicYaben <rmyl@dtu.dk>
2026-02-24 11:11:04 +0100

    mqtt

--- modules/protocols/mqtt/fingerprint.py
+++ modules/protocols/mqtt/fingerprint.py
@@ -1,17 +1,37 @@
+from copy import copy
 from dice.module import make_fp_handler, Module, new_module
 from dice.config import FINGERPRINTER
 from dice.helpers import get_record_field
+from dice.records import Service
 
 from packaging.version import parse, InvalidVersion
 
-class Broker:
-    name: str
-    version: str
-    cpe: str
-    topics: list[tuple[str, list[str]]]
+import pandas as pd
 
-def get_hub(topics: list[tuple[str, list[str]]]) -> Broker | None:
-    hub = Broker()
+BROKERS: dict[str, Service] = {
+    "vernemq": Service(
+        name="VerneMQ", 
+        vendor="Octabolabs", 
+        cpe="cpe:2.3:a:octavolabs:vernemq:*:*:*:*:*:*:*:*",
+        version=None,
+    ),
+    "mosquitto": Service(
+        name="Mosquitto", 
+        vendor="Eclipse", 
+        cpe="cpe:2.3:a:eclipse:mosquitto:*:*:*:*:*:*:*:*", 
+        version=None
+    ),
+    "activemq": Service(
+        name="ActiveMQ",
+        vendor="Apache",
+        cpe="cpe:2.3:a:apache:activemq:*:*:*:*:*:*:*:*",
+        version=None
+    ),
+
+}
+
+def get_hub(topics: list[tuple[str, list[str]]]) -> Service | None:
+    hub = Service("", None, None, None)
     for topic, msgs in topics:
         msg = msgs[0] # first message
         match topic:
@@ -23,51 +43,57 @@ def get_hub(topics: list[tuple[str, list[str]]]) -> Broker | None:
         if hub.name and hub.version:
             return hub
         
-def get_broker(topics: list[tuple[str, list[str]]]) -> Broker | None:
-    broker = Broker()
+def get_broker(topics: list[tuple[str, list[str]]]) -> Service | None:
     for topic, msgs in topics:
         match topic:
             case "$SYS/brokers":
-                if hub := get_hub(topics):
-                    return hub
+                return get_hub(topics)
             
             case s if s.startswith("$SYS/VerneMQ"):
-                broker.name = "VerneMQ"
-                broker.cpe = "cpe:2.3:a:octavolabs:vernemq:*:*:*:*:*:*:*:*"
-                return broker
+                return copy(BROKERS["vernemq"])
             
             case s if s.startswith(("$SYS/ActiveMQ", "ActiveMQ/")):
-                broker.name = "ActiveMQ"
-                broker.cpe = "cpe:2.3:a:apache:activemq:*:*:*:*:*:*:*:*"
-                return broker
+                return copy(BROKERS["activemq"])
             
             case "$SYS/broker/version":
                 v = msgs[0] if msgs else ""
 
                 if "mosquito" in v:
-                    broker.name = "mosquitto"
-                    broker.version = str(parse(v.split("mosquitto version")[1]))
-                    broker.cpe = "cpe:2.3:a:eclipse:mosquitto:*:*:*:*:*:*:*:*"
-                    return broker
+                    b = copy(BROKERS["mosquitto"])
+                    b.version = str(parse(v.split("mosquitto version")[1]))
+                    return b
                 
-                b = v.split("version")
-                broker.name = v
+                bversion = v.split("version")
+                pv = None
                 try:
-                    pv = parse(b[-1])
-                    broker.version = str(pv)
-                    broker.name = b[0]
+                    pv = parse(bversion[-1])
                 except InvalidVersion:
                     pass
+
+                broker = Service(
+                    name=bversion[0] if pv else v,
+                    version=str(pv) if pv else None,
+                    vendor=None,
+                    cpe=None,
+                )
                 return broker    
                     
 
-def fingerprint(row) -> dict | None:
+def fingerprint(row: pd.Series) -> dict | None:
     # check if we connected at all or the broker refused
     # the communication
-
-    if broker := get_broker((topics:=get_record_field(row, "topics", []))):
-        broker.topics = topics
-        return broker.__dict__
+    topics=get_record_field(row, "topics", [])
+    if not topics:
+        return
+    
+    data = {
+        "access": ["read"],
+        "authentication": "anonymous" if row.get("scheme") == "tcp" else "self-signed-certificate",
+    }
+    
+    if broker := get_broker(topics):
+        data["topics"] = topics
+        data["service"] = broker.__dict__
 
 mqtt_fp_handler = make_fp_handler(fingerprint, "mqtt")
 
